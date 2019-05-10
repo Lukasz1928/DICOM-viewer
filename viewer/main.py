d8baae24588e430edc35707a983473081f2b1e1e
Lukasz1928 <Lukasz19281@gmail.com>
2019-05-10 20:17:32 +0200

    Image edit

--- viewer/main.py
+++ viewer/main.py
@@ -1,27 +1,34 @@
 import pydicom
 import tkinter as tk
 from PIL import Image, ImageTk
+import numpy as np
+import cv2
 
 
 class MainWindow():
 
-    ds = pydicom.dcmread("./data/head.dcm")
-    data = ds.pixel_array
+    # ds = pydicom.dcmread("./data/head.dcm")
+    # data = ds.pixel_array
 
     def __init__(self, main):
         self.canvas = tk.Canvas(main, width=512, height=512)
         self.canvas.grid(row=0, column=0)
         self.canvas.bind("<Button-1>", self.initWindow)
-        self.canvas.bind("<B1-Motion>", self.updateWindow)
-        self.canvas.bind("<Button-3>", self.initMeasure)
-        self.canvas.bind("<B3-Motion>", self.updateMeasure)
-        self.canvas.bind("<ButtonRelease-3>", self.finishMeasure)
+        self.canvas.bind("<Button-3>", self.updateWindow)
+        # self.canvas.bind("<B1-Motion>", self.updateWindow)
 
-        self.array = self.data
-        self.image = Image.fromarray(self.array)
+        # self.array = self.data
+        # self.image = Image.fromarray(self.array)
+        # self.image = self.image.resize((512, 512), Image.ANTIALIAS)
+        self._img = np.ndarray([512, 512])
+        self.color = 0
+        print('dupa')
+        self._img.fill(self.color)
+        self.image = Image.fromarray(self._img)
         self.image = self.image.resize((512, 512), Image.ANTIALIAS)
         self.img = ImageTk.PhotoImage(image=self.image, master=main)
         self.image_on_canvas = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
+
 
     def transformData(self, data, window, level):
         return data
@@ -30,17 +37,16 @@ class MainWindow():
         print("x: " + str(event.x) + " y: " + str(event.y))
 
     def updateWindow(self, event):
-        print("x: " + str(event.x) + " y: " + str(event.y))
+        #print("x: " + str(event.x) + " y: " + str(event.y))
+        self._img = np.ndarray([512, 512])
+        print(self.color)
+        self._img.fill(self.color)
+        self.color = (self.color + 10) % 256
+        self.image2 = Image.fromarray(self._img)
+        self.image2 = self.image2.resize((512, 512), Image.ANTIALIAS)
+        self.img2 = ImageTk.PhotoImage(image=self.image2)
+        self.canvas.itemconfig(self.image_on_canvas, image=self.img2)
 
-
-    def initMeasure(self, event):
-        print("x: " + str(event.x) + " y: " + str(event.y))
-
-    def updateMeasure(self, event):
-        print("x: " + str(event.x) + " y: " + str(event.y))
-
-    def finishMeasure(self, event):
-        print("x: " + str(event.x) + " y: " + str(event.y))
 
 
 def main():
