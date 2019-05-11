1077f04e3c79791b3b9a222a89bd0ee6d40f0c0d
Michał Zakrzewski <miczak96@gmail.com>
2019-05-11 20:31:14 +0200

    Added menubar

--- viewer/main.py
+++ viewer/main.py
@@ -1,8 +1,9 @@
-import pydicom
 import tkinter as tk
-from PIL import Image, ImageTk
+from tkinter import filedialog
+
 import numpy as np
-import cv2
+import pydicom
+from PIL import Image, ImageTk
 
 
 class MainWindow():
@@ -10,7 +11,7 @@ class MainWindow():
     # ds = pydicom.dcmread("./data/head.dcm")
     # data = ds.pixel_array
 
-    def __init__(self, main):
+    def __init__(self, main: tk.Tk):
         self.canvas = tk.Canvas(main, width=512, height=512)
         self.canvas.grid(row=0, column=0)
         self.canvas.bind("<Button-1>", self.initWindow)
@@ -29,24 +30,37 @@ class MainWindow():
         self.img = ImageTk.PhotoImage(image=self.image, master=main)
         self.image_on_canvas = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
 
+        menubar = tk.Menu(main)
+        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu.add_command(label="Open", command=self.open_file)
+        menubar = tk.Menu(main)
+        menubar.add_cascade(label="File", menu=filemenu)
 
-    def transformData(self, data, window, level):
+        main.config(menu=menubar)
+
+    def transform_data(self, data, window, level):
         return data
 
-    def initWindow(self, event):
+    def init_window(self, event):
         print("x: " + str(event.x) + " y: " + str(event.y))
 
-    def updateWindow(self, event):
-        #print("x: " + str(event.x) + " y: " + str(event.y))
-        self._img = np.ndarray([512, 512])
-        print(self.color)
-        self._img.fill(self.color)
-        self.color = (self.color + 10) % 256
-        self.image2 = Image.fromarray(self._img)
-        self.image2 = self.image2.resize((512, 512), Image.ANTIALIAS)
-        self.img2 = ImageTk.PhotoImage(image=self.image2)
-        self.canvas.itemconfig(self.image_on_canvas, image=self.img2)
+    def open_file(self):
+        path = filedialog.askopenfilename(initialdir=".", title="Select file",
+                                          filetypes=(("DICOM files", "*.dcm"),))
+        if path != '':
+            self._img = pydicom.dcmread(path).pixel_array
+            self.updateWindow()
 
+    def update_window(self, event=None):
+        # print("x: " + str(event.x) + " y: " + str(event.y))
+        # self._img = np.ndarray([512, 512])
+        # print(self.color)
+        # self._img.fill(self.color)
+        # self.color = (self.color + 10) % 256
+        image2 = Image.fromarray(self._img)
+        image2 = image2.resize((512, 512), Image.ANTIALIAS)
+        img2 = ImageTk.PhotoImage(image=image2)
+        self.canvas.itemconfig(self.image_on_canvas, image=img2)
 
 
 def main():
