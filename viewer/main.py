ded28811984a1e3fca0bb41eb364e3704a8379a6
Lukasz1928 <Lukasz19281@gmail.com>
2019-05-19 11:51:42 +0200

    Added simple drawing

--- viewer/main.py
+++ viewer/main.py
@@ -5,62 +5,71 @@ import numpy as np
 import pydicom
 from PIL import Image, ImageTk
 
+from viewer.drawer import Drawer
 
-class MainWindow():
 
-    # ds = pydicom.dcmread("./data/head.dcm")
-    # data = ds.pixel_array
+class MainWindow:
 
     def __init__(self, main: tk.Tk):
+        self.main = main
         self.canvas = tk.Canvas(main, width=512, height=512)
-        self.canvas.grid(row=0, column=0)
-        self.canvas.bind("<Button-1>", self.initWindow)
-        self.canvas.bind("<Button-3>", self.updateWindow)
-        # self.canvas.bind("<B1-Motion>", self.updateWindow)
+        self.drawer = Drawer(self.canvas)
+        self.canvas.grid(row=1, column=0)
 
-        # self.array = self.data
-        # self.image = Image.fromarray(self.array)
-        # self.image = self.image.resize((512, 512), Image.ANTIALIAS)
+        self.setup_default_bindings()
+        self.setup_initial_image()
+        self.setup_menubar()
+        self.setup_menu()
+
+    def setup_default_bindings(self):
+        self.canvas.unbind("<B1-Motion>")
+        self.canvas.unbind("<ButtonRelease-1>")
+
+    def setup_initial_image(self):
         self._img = np.ndarray([512, 512])
-        self.color = 0
-        print('dupa')
+        self.color = 255
         self._img.fill(self.color)
         self.image = Image.fromarray(self._img)
         self.image = self.image.resize((512, 512), Image.ANTIALIAS)
-        self.img = ImageTk.PhotoImage(image=self.image, master=main)
+        self.img = ImageTk.PhotoImage(image=self.image, master=self.main)
         self.image_on_canvas = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
 
-        menubar = tk.Menu(main)
+    def setup_menubar(self):
+        menubar = tk.Menu(self.main)
         filemenu = tk.Menu(menubar, tearoff=0)
         filemenu.add_command(label="Open", command=self.open_file)
-        menubar = tk.Menu(main)
+        menubar = tk.Menu(self.main)
         menubar.add_cascade(label="File", menu=filemenu)
+        self.main.config(menu=menubar)
 
-        main.config(menu=menubar)
+    def setup_drawing_bindings(self):
+        self.canvas.bind("<B1-Motion>", self.drawer.draw_curve)
+        self.canvas.bind("<ButtonRelease-1>", self.drawer.reset_curve)
 
-    def transform_data(self, data, window, level):
-        return data
+    def draw_button_command(self):
+        if self.b.config('relief')[-1] == 'sunken':
+            self.setup_default_bindings()
+            self.b.config(relief="raised")
+        else:
+            self.setup_drawing_bindings()
+            self.b.config(relief="sunken")
 
-    def init_window(self, event):
-        print("x: " + str(event.x) + " y: " + str(event.y))
+    def setup_menu(self):
+        self.b = tk.Button(self.main, text="Draw", command=self.draw_button_command, relief="raised")
+        self.b.grid(row=0, column=0)
 
     def open_file(self):
         path = filedialog.askopenfilename(initialdir=".", title="Select file",
                                           filetypes=(("DICOM files", "*.dcm"),))
         if path != '':
             self._img = pydicom.dcmread(path).pixel_array
-            self.updateWindow()
+            self.update_window()
 
     def update_window(self, event=None):
-        # print("x: " + str(event.x) + " y: " + str(event.y))
-        # self._img = np.ndarray([512, 512])
-        # print(self.color)
-        # self._img.fill(self.color)
-        # self.color = (self.color + 10) % 256
-        image2 = Image.fromarray(self._img)
-        image2 = image2.resize((512, 512), Image.ANTIALIAS)
-        img2 = ImageTk.PhotoImage(image=image2)
-        self.canvas.itemconfig(self.image_on_canvas, image=img2)
+        self.image2 = Image.fromarray(self._img)
+        self.image2 = self.image2.resize((512, 512), Image.ANTIALIAS)
+        self.img2 = ImageTk.PhotoImage(image=self.image2)
+        self.canvas.itemconfig(self.image_on_canvas, image=self.img2)
 
 
 def main():
